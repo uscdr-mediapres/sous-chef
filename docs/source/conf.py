f48ad8d2dc5edd8cbfe4a5acf3827e3955e23ad0
Shalini <saiprasa@usc.edu>
2024-12-17 15:33:50 -0800

    docs test 3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,8 @@ parent_directory = os.path.dirname(os.path.dirname(os.path.dirname(current_direc
 scripts_directory = os.path.join(parent_directory, "scripts")
 gui_directory = os.path.join(parent_directory, "GUI")
 sys.path.insert(0, scripts_directory)
+sys.path.insert(0, gui_directory)
+
 project = "USC-DR-RAWCOOKED"
 copyright = "2024, Shalini Sai Prasad, Rajat Shrivastava, Trinanjan Nandi"
 author = "Shalini Sai Prasad, Rajat Shrivastava, Trinanjan Nandi"
